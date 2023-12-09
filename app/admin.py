a0e94d771f90c1aca25b95b0022bd2b266a10d07
p.gerasimov <gerasimov86@gmail.com>
2023-12-09 17:28:52 +0300

    add gitignore, dell comments in admin.py

--- app/admin.py
+++ app/admin.py
@@ -4,23 +4,19 @@ from .models import Course, Teacher, Student, Schedule
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    # Настройки для модели Course, если необходимо
     pass
 
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    # Настройки для модели Teacher, если необходимо
     pass
 
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    # Настройки для модели Student, если необходимо
     pass
 
 
 @admin.register(Schedule)
 class ScheduleAdmin(admin.ModelAdmin):
-    # Настройки для модели Schedule, если необходимо
     pass
